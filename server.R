cd88202502c209e655d82f3fe0c7bb1b4a23aebe
wildcamping <stuartbarker@Stuarts-MBP.connect>
2020-01-20 20:22:11 +0000

    initial commit

---
+++ server.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(leaflet)
+library(leaflet.extras)
+library(data.table)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+    md <- fread("treelinks.csv")
+    md <- md[sample(nrow(md), 1), ]
+    output$example <- renderUI({
+        tags$a(imageOutput("bamboo-forest-nature-green.jpg"),href=paste0("https://en.wikipedia.org",md$url))
+    })
+        output$example2 <- renderUI({
+            tags$a(imageOutput("bamboo-forest-nature-green.jpg"),href=paste0("https://en.wikipedia.org",md$url))
+    })
+    
+    })
+
---
+++ ui.R
@@ -0,0 +1,29 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinyjs)
+library(shinyWidgets)
+
+ui <- fillPage(fluidPage(
+    titlePanel(h2("The Tree ID", align = "center")),
+    #titlePanel(h4("Got 5 minutes?", align = "center")),
+    titlePanel(h4("Click the image to be taken to a random wikipedia tree page...", align = "center")),
+    titlePanel(h5(    em(
+        span("Created by "),
+        a("The Tree ID", href = "mailto:barkerstu@gmail.com"),
+        span(", Jan 2020")), align = "center")),
+    uiOutput("example"),
+                setBackgroundImage(
+                    src = "forest-trees-minimal-art.jpg"),
+                uiOutput("example2")
+
+
+    )
+)
