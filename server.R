c23fe494859b137e5f21eb5f70b2e05a4597db84
wildcamping <stuartbarker@Stuarts-MBP.connect>
2020-04-13 16:02:23 +0100

    fixed for shiny server

--- server.R
+++ server.R
@@ -1,22 +1,21 @@
 library(shiny)
 library(leaflet)
 library(leaflet.extras)
-library(data.table)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    md <- fread("treelinks.csv")
+    md <- read.csv("treelinks.csv")
     md <- md[sample(nrow(md), 1),]
     output$example <- renderUI({
         tags$a(
             imageOutput("bamboo-forest-nature-green.jpg"),
-            href = paste0("https://en.wikipedia.org", md$url)
+            href = paste0("https://en.wikipedia.org", md[1])
         )
     })
     output$example2 <- renderUI({
         tags$a(
             imageOutput("bamboo-forest-nature-green.jpg"),
-            href = paste0("https://en.wikipedia.org", md$url)
+            href = paste0("https://en.wikipedia.org", md[1])
         )
     })
     
